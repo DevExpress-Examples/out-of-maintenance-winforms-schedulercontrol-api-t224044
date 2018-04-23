b0bac9d79ff8e2f45930a14d59bc81ccb331523b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SchedulerAPISample/CodeExamples/Time Cell Examples.cs
@@ -0,0 +1,79 @@
+﻿using DevExpress.XtraScheduler;
+using DevExpress.XtraScheduler.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulerAPISample.CodeExamples {
+    class TimeCellExamples {
+
+        static void TimeCellTypes(SchedulerControl scheduler) {
+            #region #TimeCellTypes
+            scheduler.CustomDrawTimeCell += scheduler_CustomDrawTimeCell;
+            scheduler.CustomDrawDayViewAllDayArea += Scheduler_CustomDrawDayViewAllDayArea;
+            scheduler.TimelineView.GetBaseTimeScale().Width = 150;
+            scheduler.WeekView.Enabled = true;
+            scheduler.DayView.DayCount = 3;
+            scheduler.ActiveViewType = SchedulerViewType.Day;
+            scheduler.GroupType = SchedulerGroupType.Date;
+            scheduler.DateNavigationBar.Visible = false;
+            scheduler.LookAndFeel.UseDefaultLookAndFeel = false;
+            scheduler.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Skin;
+            scheduler.LookAndFeel.SkinName = "DevExpress Style";
+            #endregion #TimeCellTypes
+        }
+
+        #region #@TimeCellTypes
+        public static void scheduler_CustomDrawTimeCell(object sender, CustomDrawObjectEventArgs e) {
+            e.DrawDefault();
+            DrawCellInfo(e);
+            e.Handled = true;
+        }
+        private static void Scheduler_CustomDrawDayViewAllDayArea(object sender, CustomDrawObjectEventArgs e) {
+            e.DrawDefault();
+            DrawCellInfo(e);
+            e.Handled = true;
+        }
+        private static void DrawCellInfo(CustomDrawObjectEventArgs e) {
+            string s = e.ObjectInfo.GetType().ToString().Substring("DevExpress.XtraScheduler.Drawing.".Length);
+            SchedulerViewCellBase cell = e.ObjectInfo as SchedulerViewCellBase;
+            if (cell != null) {
+                StringFormat sf = new StringFormat();
+                sf.Alignment = StringAlignment.Center;
+                sf.LineAlignment = StringAlignment.Center;
+                e.Cache.DrawString(s, new Font("Tahoma", 10), SystemBrushes.GrayText, cell.Bounds, sf);
+            }
+        }
+        #endregion #@TimeCellTypes
+
+        static void HorizontalSingleWeekCellType(SchedulerControl scheduler) {
+            #region #HorizontalSingleWeekCellType
+            scheduler.CustomDrawTimeCell += scheduler_CustomDrawTimeCell_01;
+            scheduler.ActiveViewType = SchedulerViewType.Month;
+            scheduler.GroupType = SchedulerGroupType.Date;
+            scheduler.DateNavigationBar.Visible = false;
+            scheduler.LookAndFeel.UseDefaultLookAndFeel = false;
+            scheduler.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Skin;
+            scheduler.LookAndFeel.SkinName = "DevExpress Style";
+            #endregion #HorizontalSingleWeekCellType
+        }
+
+        #region #@HorizontalSingleWeekCellType
+        public static void scheduler_CustomDrawTimeCell_01(object sender, CustomDrawObjectEventArgs e) {
+            e.DrawDefault();
+            HorizontalSingleWeekCell cell = e.ObjectInfo as HorizontalSingleWeekCell;
+            if (cell != null) {
+                StringFormat sf = new StringFormat();
+                sf.LineAlignment = StringAlignment.Center;
+                e.Cache.DrawRectangle(SystemPens.ActiveBorder, cell.Bounds);
+                if (cell.FirstVisible) e.Cache.DrawString("First Visible Cell", new Font("Tahoma", 8),
+                        Brushes.Blue, cell.Bounds, sf);
+            }
+            e.Handled = true;
+        }
+        #endregion #@HorizontalSingleWeekCellType
+    }
+}
---
+++ VB/SchedulerAPISample/CodeExamples/AppointmentExamples.vb
@@ -0,0 +1,58 @@
+﻿Imports DevExpress.XtraScheduler
+Imports System
+
+Namespace SchedulerAPISample.CodeExamples
+    Friend Class AppointmentExamples
+
+        Private Shared Sub CreateAppointmentFromSelection(ByVal scheduler As SchedulerControl)
+'            #Region "#CreateAppointmentFromSelection"
+            ' Delete all appointments.
+            scheduler.Storage.Appointments.Clear()
+            ' Select time interval
+            scheduler.ActiveView.SetSelection(New TimeInterval(Date.Now, New TimeSpan(2, 40, 0)), scheduler.ActiveView.GetResources()(1))
+            ' Group by resource.
+            scheduler.GroupType = SchedulerGroupType.Resource
+            ' Create a new appointment.
+            Dim apt As Appointment = scheduler.Storage.CreateAppointment(AppointmentType.Normal)
+
+            ' Set the appointment's time interval to the selected time interval.
+            apt.Start = scheduler.SelectedInterval.Start
+            apt.End = scheduler.SelectedInterval.End
+
+            ' Set the appointment's resource to the resource which contains
+            ' the currently selected time interval.
+            apt.ResourceId = scheduler.SelectedResource.Id
+
+            ' Add the new appointment to the appointment collection.
+            scheduler.Storage.Appointments.Add(apt)
+'            #End Region ' #CreateAppointmentFromSelection
+        End Sub
+
+        Private Shared Sub UseCustomFields(ByVal scheduler As SchedulerControl)
+'            #Region "#UseCustomFields"
+            scheduler.Storage.Appointments.Clear()
+            ' Handle the FilterAppointment event to display appointments by some criteria.
+            AddHandler scheduler.Storage.FilterAppointment, AddressOf Storage_FilterAppointment
+            scheduler.DayView.TopRowTime = TimeSpan.FromHours(Date.Now.Hour)
+
+            ' Add mapping to create a custom field. 
+            ' If Scheduler is bound to data, the string "DataFieldOne" should match the field name in the data source.
+            ' For unbound Scheduler, the data field name is required but not used.
+            scheduler.Storage.Appointments.CustomFieldMappings.Add(New AppointmentCustomFieldMapping("PropertyOne", "DataFieldOne"))
+            Dim apt1 As Appointment = scheduler.Storage.CreateAppointment(AppointmentType.Normal, Date.Now, New TimeSpan(1, 0, 0), "Visible Appointment")
+            apt1.CustomFields("PropertyOne") = "Visible"
+            scheduler.Storage.Appointments.Add(apt1)
+            Dim apt2 As Appointment = scheduler.Storage.CreateAppointment(AppointmentType.Normal, Date.Now.AddHours(2), New TimeSpan(0, 30, 0), "Hidden Appointment")
+            apt2.CustomFields("PropertyOne") = "Hidden"
+            scheduler.Storage.Appointments.Add(apt2)
+'            #End Region ' #UseCustomFields
+        End Sub
+
+        #Region "#@UseCustomFields"
+        Private Shared Sub Storage_FilterAppointment(ByVal sender As Object, ByVal e As PersistentObjectCancelEventArgs)
+            e.Cancel = CType(e.Object, Appointment).CustomFields("PropertyOne").ToString() = "Hidden"
+        End Sub
+        #End Region ' #@UseCustomFields
+
+    End Class
+End Namespace
